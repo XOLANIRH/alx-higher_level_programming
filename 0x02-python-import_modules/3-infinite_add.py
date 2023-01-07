07070117a9748286f35ae01e61adcef572e641a3
XOLANIRH <xolanirh@gmail.com>
2023-01-07 02:17:09 +0200

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
